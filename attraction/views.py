2b48a78b372619f5b45e712c60a70cc252c2c252
zhangke <906879240@qq.com>
2022-05-01 15:00:12 +0800

    feat: attractions sorting function

--- attraction/views.py
+++ attraction/views.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -12,9 +14,24 @@ class AttractionViewSet(ModelViewSet):
     queryset = Attraction.objects.all()
 
     def list(self, request, **kwargs):
+
         queryset = Attraction.objects.all().filter(expiration_time=None)
-        serializer = AttractionSerializer(queryset, many=True)
+        if len(self.request.query_params) != 0:
+            scenery = self.request.query_params.get("scenery")
+            shopping = self.request.query_params.get("shopping")
+            repast = self.request.query_params.get("repast")
+            entertainment = self.request.query_params.get("entertainment")
+            accommodation = self.request.query_params.get("accommodation")
+            traffic = self.request.query_params.get("traffic")
+            res_list = sorted(list(queryset), key=lambda x: -(
+                    x.scenery_score * Decimal(scenery) +
+                    x.shopping_score * Decimal(shopping) +
+                    x.repast_score * Decimal(repast) +
+                    x.entertainment_score * Decimal(entertainment) +
+                    x.accommodation_score * Decimal(accommodation) +
+                    x.traffic_score * Decimal(traffic)))
+            serializer = AttractionSerializer(res_list, many=True)
+        else:
+            serializer = AttractionSerializer(queryset, many=True)
         response = {'code': 0, 'data': serializer.data, 'msg': '', 'total': len(serializer.data)}
         return Response(response)
-
-
