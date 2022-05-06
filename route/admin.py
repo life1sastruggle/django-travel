d4b7cb54f302c3c09f8bd4c9aad23a6cce61ee30
zhangke <906879240@qq.com>
2022-05-06 20:06:26 +0800

    feat: add admin for RouteAttractionMapping

--- route/admin.py
+++ route/admin.py
@@ -1,14 +1,22 @@
 from django.contrib import admin
 
-from route.models import Route
+from route.models import Route, RouteAttractionMapping
 
 
 class RouteAdmin(admin.ModelAdmin):
-    list_per_page = 5
+    list_per_page = 10
     list_display = ('id', 'name', 'price', 'introduction', 'image')
     list_editable = ('name', 'price', 'introduction', 'image')
     search_fields = ('name',)
     fields = ('name', 'price', 'introduction', 'image')
 
 
+class RouteAttractionAdmin(admin.ModelAdmin):
+    list_per_page = 10
+    list_display = ('id', 'route', 'attraction')
+    list_editable = ('route', 'attraction')
+    fields = ('route', 'attraction')
+
+
 admin.site.register(Route, RouteAdmin)
+admin.site.register(RouteAttractionMapping, RouteAttractionAdmin)
